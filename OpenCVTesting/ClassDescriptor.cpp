abb42ed6c6d5f91b838988c94c67038b04b7651a
NoobsDeSroobs <magnus_elden@hotmail.com>
2015-11-07 02:14:21 +0100

    Added the refactored files.

---
+++ OpenCVTesting/ClassDescriptor.cpp
@@ -0,0 +1,16 @@
+﻿#include "ClassDescriptor.h"
+
+void ClassDescriptor::addPixel(std::vector<float> pixelFeatures)
+{
+	for (size_t i = 0; i < pixelFeatures.size(); i++) {
+		numClassPixels[i]++;
+		Descriptor[i] += pixelFeatures[i];
+	}
+}
+
+void ClassDescriptor::CalculateDescriptor()
+{
+	for (size_t i = 0; i < Descriptor.size(); i++) {
+		Descriptor[i] = Descriptor[i] / numClassPixels[i];
+	}
+}
---
+++ OpenCVTesting/ClassDescriptor.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include <vector>
+#include <opencv2/core/core.hpp>
+
+class ClassDescriptor
+{
+public:
+	std::vector<int> numClassPixels;
+	std::vector<float> Descriptor;
+	size_t num_features_t_;
+
+	explicit ClassDescriptor(size_t num_features_t)
+		: num_features_t_(num_features_t)
+	{
+		numClassPixels.resize(num_features_t_, 0);
+		Descriptor.resize(num_features_t_, 0);;
+	}
+
+	void addPixel(std::vector<float> pixelFeatures);
+
+	void CalculateDescriptor();
+	
+};
